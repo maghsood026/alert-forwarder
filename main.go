9384b109b9cf8ac0ef99063c463cc1ce011cb1c0
maghsood <maghsood95@gmail.com>
2025-04-22 11:07:31 +0330

    change request type from struc to pour form

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -31,13 +34,33 @@ type AlertManagerPayload struct {
 	GroupKey          string            `json:"groupKey"`
 }
 
+type EnQueue map[string]interface{}
 type SMSRequestFormat struct {
-	Message string `json:"message"`
-	Target  string `json:"target"` // destination phone number
+	requestBody map[string][]EnQueue
 }
 type ResponseFormat map[string]string
 
 const SMSWebhook string = "https://sms.org"
+
+func setEnv() {
+
+	envFile, err := os.Open("./.env")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer envFile.Close()
+
+	scanner := bufio.NewScanner(envFile)
+
+	for scanner.Scan() {
+		envVar := strings.Split(scanner.Text(), "=")
+		os.Setenv(envVar[0], envVar[1])
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
 
 func fetchResponseSample(c *gin.Context) {
 	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
@@ -55,17 +78,20 @@ func fetchResponseSample(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusOK, data)
 }
-func sendSMS(annotations map[string]string) ResponseFormat {
-	var responseData ResponseFormat
-
-	smsInstance := SMSRequestFormat{
-		Message: annotations["description"],
-		Target:  annotations["target"],
+func sendSMS(annotations map[string]string) map[string]string {
+	data := map[string]interface{}{
+		"enqueue": []map[string]interface{}{
+			{
+				"Destination": "9333333333",
+				"Message":     "salam jigar",
+				"Originator":  os.Getenv("ORG_NUM"),
+			},
+		},
 	}
-
-	postBody, _ := json.Marshal(smsInstance)
+	postBody, _ := json.Marshal(data)
 	requestBody := bytes.NewBuffer(postBody)
 
+	var responseData map[string]string
 	resp, err := http.Post(SMSWebhook, "application/json", requestBody)
 	if err != nil {
 		log.Fatalln(err)
@@ -82,26 +108,26 @@ func sendSMS(annotations map[string]string) ResponseFormat {
 }
 func sendRequestToTargetUser(c *gin.Context) {
 	var request AlertManagerPayload
-	var response ResponseFormat
+	var response map[string]string
 	c.BindJSON(&request)
 	annotations := request.Alerts[0].Annotations
 
 	switch sendingMethod := annotations["sending-method"]; sendingMethod {
 	case "sms":
-		// response = sendSMS(annotations)
-		// c.IndentedJSON(http.StatusOK, response)
-		c.IndentedJSON(http.StatusOK, map[string]string{"response": "every this is okey"}) // test api!
+		response = sendSMS(annotations)
+		c.IndentedJSON(http.StatusOK, response) // test api!
 	case "call":
 		// call user will implement soon ...
 	default:
-		response = ResponseFormat{
+		DefaultResponse := ResponseFormat{
 			"message": "sending method not defined",
 		}
-		c.IndentedJSON(http.StatusBadRequest, response)
+		c.IndentedJSON(http.StatusBadRequest, DefaultResponse)
 	}
 }
 
 func main() {
+	setEnv()
 	router := gin.Default()
 	router.GET("/fetch-action", fetchResponseSample)
 	router.POST("/request-action", sendRequestToTargetUser)
